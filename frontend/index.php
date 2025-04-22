b347ba37a73e5bc8551d0959e0e4bca234f183f0
IvanPSG-BR <ivanpedrosampaio@gmail.com>
2025-04-22 01:40:39 -0300

    Commit Inicial
    - Página index em desenvolvimento

---
+++ frontend/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Agendador de Tarefas gamificado no estilo RPG Medieval">
+
+        <link rel="stylesheet" href="assets/css/style.css">
+
+        <title>Tasksmith - Construa suas Tarefas!</title>
+        <link rel="icon" href="assets/favicons/favicon.ico">
+    </head>
+
+    <body>
+        <header id="home_hdr">
+            <button class="no_bg_btn"><img src="assets/images/modo-claro.webp" alt=""></button>
+            <nav class="home_nav">
+                <div id="logo">
+                    <h1>Tasksmith</h1>
+                    <button class="no_bg_btn"><img src="assets/images/anvil.webp" id="anvil" alt="anvil_emoji"></button>
+                </div>
+
+                <ul id="auth_section">
+                    <li><a href=""><button class="wooden_sign auth_btn">Entrar</button></a></li>
+                    <li><a href=""><button class="wooden_sign auth_btn">Cadastrar</button></a></li>
+                </ul>
+            </nav>
+        </header>
+
+        <main>
+            Placeholder
+        </main>
+
+        <footer id="home_ftr">
+            <nav class="home_nav">
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </footer>
+    </body>
+</html>
