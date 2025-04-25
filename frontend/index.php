59bff0de13ba459bbc94149264e3ee522298d620
IvanPSG-BR <ivanpedrosampaio@gmail.com>
2025-04-25 04:39:51 -0300

    Atualização na estrutura do front-end e Refatoração do código
    - Protótipo da Landing Page
    - HTML (em construção) com base no protótipo
    - Maior parte do CSS apagado para ser refeito e reorganizado
    - Ícones e elementos UI (gratuitos) adicionados ao projeto

--- frontend/index.php
+++ frontend/index.php
@@ -8,33 +8,82 @@
         <link rel="stylesheet" href="assets/css/style.css">
 
         <title>Tasksmith - Construa suas Tarefas!</title>
-        <link rel="icon" href="assets/favicons/favicon.ico">
+        <link rel="icon" href="favicon.ico">
     </head>
 
     <body>
         <header id="home_hdr">
-            <button class="no_bg_btn"><img src="assets/images/modo-claro.webp" alt=""></button>
             <nav class="home_nav">
-                <div id="logo">
-                    <h1>Tasksmith</h1>
-                    <button class="no_bg_btn"><img src="assets/images/anvil.webp" id="anvil" alt="anvil_emoji"></button>
-                </div>
+                <ul id="logo">
+                    <li><img src="assets/images/icons/notepad.webp" alt="" id="notepad"></li>
+                    <li><h1>Tasksmith</h1></li>
+                    <li><button class="no_bg_btn"><img src="assets/images/icons/anvil.webp" id="anvil" alt="anvil_emoji"></button></li>
+                </ul>
 
-                <ul id="auth_section">
+                <ul id="menu">
+                    <li><a href="">Sobre Nós</a></li>
+                    <li><a href="">Páginas</a></li>
+                    <li><a href="">Contato</a></li>
+                </ul>
+
+                <ul id="auth">
                     <li><a href=""><button class="wooden_sign auth_btn">Entrar</button></a></li>
-                    <li><a href=""><button class="wooden_sign auth_btn">Cadastrar</button></a></li>
                 </ul>
             </nav>
         </header>
 
         <main>
-            Placeholder
+            <section id="cta">
+
+            </section>
+
+            <section id="lore">
+
+            </section>
+
+            <section id="imgs">
+
+            </section>
+
+            <section id="features">
+
+            </section>
+
+            <section id="faq">
+
+            </section>
+
+            <section id="community">
+
+            </section>
         </main>
 
         <footer id="home_ftr">
-            <nav class="home_nav">
+            <nav>
                 <ul>
-                    <li></li>
+                    <li>Linha</li>
+                    <li class="home_nav">
+                        <span>
+                            <h6>Conteúdo</h6>
+                            <h6>Do</h6>
+                            <h6>Footer</h6>
+                        </span>
+
+                        <span>
+                            <h6>Mais</h6>
+                            <h6>Conteúdo</h6>
+                            <h6>Do Footer</h6>
+                        </span>
+
+                        <span>
+                            <h6>Ainda mais</h6>
+                            <h6>Conteúdo</h6>
+                            <h6>Do Footer</h6>
+                        </span>
+                    </li>
+                    <li>Linha</li>
+
+                    <li>Todos os direitos reservados</li>
                 </ul>
             </nav>
         </footer>
