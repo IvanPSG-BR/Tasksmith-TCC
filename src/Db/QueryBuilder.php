7f4835b0286b6301c45e0a8420411669f154d878
IvanPSG-BR <ivanpedrosampaio@gmail.com>
2025-07-05 14:48:23 -0300

    Implementação e refatoração métodos CRUD no QueryBuilder
    - Implementação dos métodos `insert`, `update` e `delete` na classe `QueryBuilder` para gerenciar operações CRUD no banco de dados.
    - Refatoração do método `select` existente para aprimorar a segurança contra SQL Injection, utilizando prepared statements para a cláusula `LIKE`. Centralização da lógica de execução de queries em um método `execute` privado para maior reusabilidade e consistência no tratamento de erros.
    - Adição de validações para garantir que os arrays de campos e valores sejam unidimensionais nos métodos `insert` e `update`, prevenindo erros e melhorando a robustez.

--- src/Db/QueryBuilder.php
+++ src/Db/QueryBuilder.php
@@ -7,8 +7,10 @@ use PDOStatement;
 
 class QueryBuilder {
     private PDO $database;
+    private $table;
 
     public function __construct($table) {
+        $this->table = $table;
         $this->set_conn();
     }
 
@@ -31,7 +33,7 @@ class QueryBuilder {
         }
     }
 
-    public function select(array $values, string $table, array $extra_keywords = null, string $distinct_value = null, string $condition = null, string $order_value = null, string $search_pattern = null) {
+    public function select(array $values, array $extra_keywords = null, string $distinct_value = null, string $condition = null, string $order_value = null, string $search_pattern = null) {
         $query = "SELECT ";
         $bindings = [];
 
@@ -39,18 +41,18 @@ class QueryBuilder {
             $query .= "DISTINCT " . $distinct_value . " ";
         }
 
-        $query .= implode(", ", $values) . " FROM " . $table;
+        $query .= implode(", ", $values) . " FROM " . $this->table;
 
         if ($search_pattern) {
             if ($condition) {
-                $query .= " WHERE " . $condition . " LIKE ?";
+                $query .= " WHERE " . "($condition)" . " LIKE ?";
                 $bindings[] = "%" . $search_pattern . "%";
             } else {
                 error_log("Erro: search_pattern fornecido sem uma coluna para a condição LIKE.");
                 return false;
             }
         } else if ($condition) {
-            $query .= " WHERE " . $condition;
+            $query .= " WHERE " . "($condition)";
         }
 
         if ($extra_keywords) {
@@ -76,8 +78,8 @@ class QueryBuilder {
         return false;
     }
 
-    public function insert(string $table, array $fields, array $values) {
-        $query = "INSERT INTO $table ";
+    public function insert(array $fields, array $values) {
+        $query = "INSERT INTO $this->table ";
         $value_bindings = array_pad([], count($values), "?");
 
         if (count($fields) != count($values)) {
@@ -95,6 +97,44 @@ class QueryBuilder {
 
         if ($result) {
             return $this->database->lastInsertId();
+        }
+        return false;
+    }
+
+    public function update(array $fields, array $values, string $condition) {
+        $query = "UPDATE $this->table SET ";
+        $set_parts = [];
+
+        if (count($fields) != count($values)) {
+            error_log("Erro: Número de campos passados diferente do número de valores");
+            return false;
+        }
+        if (!array_is_list($fields) || !array_is_list($values)) {
+            error_log("Erro: Campos e valores devem ser arrays unidimensionais");
+            return false;
+        }
+
+        foreach ($fields as $index => $field) {
+            $set_parts[] = "$field = ?";
+        }
+        $query .= implode(", ", $set_parts);
+        $query .= " WHERE ($condition);";
+
+        $result = $this->execute($query, $values);
+
+        if ($result) {
+            return true;
+        }
+        return false;
+    }
+
+    public function delete(string $condition) {
+        $query = "DELETE FROM " . $this->table . " WHERE ($condition);";
+
+        $result = $this->execute($query);
+
+        if ($result) {
+            return true;
         }
         return false;
     }
