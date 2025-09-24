e6f099a8ae6fc1893b91517ff60cf9c3b6632cb7
IvanPSG-BR <ivanpedrosampaio@gmail.com>
2025-09-24 18:19:06 -0300

    Pequenas alterações
    - Começo da Implementação da página de cadastro

--- src/Views/auth/signup.php
+++ src/Views/auth/signup.php
@@ -1,2 +1,37 @@
-<?= "Página de cadastro"?>
-<a href="/login"><br>tela de login (TESTE)</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php $assets = "src/assets";?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tasksmith - Login</title>
+
+    <link rel="stylesheet" href=<?=$assets . "/css/style.css"?>>
+    <link rel="stylesheet" href=<?=$assets . "/css/pages/signup.css"?>>
+</head>
+<body>
+    <div id="container" class="flex">
+        <form action="/login" method="post">
+            <div id="pag_esq">
+                <div class="campo-form">
+                    <label for="username">Usuário:</label>
+                    <input type="text" name="username" id="username">
+                </div>
+                <div class="campo-form">
+                    <label for="password">Senha:</label>
+                    <input type="password" name="password" id="password">
+                </div>
+                <div class="campo-form">
+                    <label for="pass_confirm">Confirme a Senha</label>
+                    <input type="password" name="pass_confirm" id="pass_confirm">
+                </div>
+            </div>
+            <div id="pag_dir">
+                <div class="btn-form">
+                    <button type="submit">NOVO JOGO</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
